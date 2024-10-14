ea9b2fdb3e4100acccef9b1eff8318ccd74422d6
Caspersonn <lucakasper8@gmail.com>
2024-10-14 16:18:05 +0200

    Adding plugin to neovim

--- home/neovim.nix
+++ home/neovim.nix
@@ -3,14 +3,22 @@
     neovim = {
 	    enable = true;
   	    defaultEditor = true;
-        extraConfig = lib.fileContents ../home/dotfiles/init.vim;
 	    viAlias = true;
 	    vimAlias = true;
-		plugins = with pkgs.vimPlugins; 
-		   [
+		plugins = with pkgs.vimPlugins; [
 		     neo-tree-nvim
-		     tmux-nvim 
-		   ];
-	   }; 
+			 tmux-nvim
+		     (pkgs.vimPlugins.buildVimPluginFrom2Nix {
+               pname = "neovim-like-vscode";
+                version = "1.0.0";  # You can update this with the actual version or commit hash you want
+                src = pkgs.fetchFromGitHub {
+                  owner = "josethz00";
+                  repo = "neovim-like-vscode";
+                  rev = "main";  # or a specific commit hash
+                  sha256 = "sha256-16sgvv5h7fkkf0qc2k5k9fzdkip8ssk4zmzw30i1i9q170p1rnhv";  # replace this with the actual sha256 value
+             };
+            })
+		];
+	}; 
 }
 
