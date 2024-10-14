44f17f117f18f3c770197157ed5e45db61baeebe
Caspersonn <lucakasper8@gmail.com>
2024-10-14 16:43:03 +0200

    Flake.nix fix

--- home/neovim.nix
+++ home/neovim.nix
@@ -15,7 +15,7 @@
                 owner = "josethz00";
                 repo = "neovim-like-vscode";
                 rev = "main";  # Specify the branch or commit hash
-                sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";  # Replace with actual sha256
+                sha256 = "sha256-G9ocLjgBpxgiGPzXT6bW6MbZvkuzTMEwcHO6A8veT5s=";  # Replace with actual sha256
                };
             })
 		];
