88bfb4270cd57805ee9cbe89177de8ae3205f2fc
Luca Kasper <lucakasper8@gmail.com>
2024-10-15 09:53:02 +0200

    Add dotifiles with fix

--- home/neovim.nix
+++ home/neovim.nix
@@ -1,11 +1,15 @@
-{config, pkgs, ...}: {
-
+{config, pkgs, ...}: 
+let 
+   neovim-like-vscode = ./dotfiles/.nvim
+in
+{
    programs.neovim = {
 	    enable = true;
   	    defaultEditor = true;
 	    viAlias = true;
 	    vimAlias = true;
-		plugins = with pkgs.vimPlugins; [
+            extraConfig = builtins.readFile "${neovim-like-vscode}/init.vim";
+            plugins = with pkgs.vimPlugins; [
 		neo-tree-nvim
 	  	tmux-nvim
 	    	];
