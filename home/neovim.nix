dcde2e7ff6d15acc005f60cd41329ef2538eca1d
Luca Kasper <lucakasper8@gmail.com>
2024-10-15 09:33:32 +0200

    Removed dotfiles

--- home/neovim.nix
+++ home/neovim.nix
@@ -6,19 +6,8 @@
 	    viAlias = true;
 	    vimAlias = true;
 		plugins = with pkgs.vimPlugins; [
-		     neo-tree-nvim
-			 tmux-nvim
-			 (pkgs.vimUtils.buildVimPlugin {
-               pname = "neovim-like-vscode";
-               version = "1.0.0";
-               src = pkgs.fetchFromGitHub {
-                owner = "josethz00";
-                repo = "neovim-like-vscode";
-                rev = "main";  # Specify the branch or commit hash
-                sha256 = "sha256-G9ocLjgBpxgiGPzXT6bW6MbZvkuzTMEwcHO6A8veT5s=";  # Replace with actual sha256
-               };
-            })
-		];
-	}; 
+		neo-tree-nvim
+	  	tmux-nvim
+	    	];
+  }; 
 }
-
