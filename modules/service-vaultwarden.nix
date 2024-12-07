a3645fb704308cf0c6942f5d8d5aeb41cc8706a7
Luca Kasper <96787412+Caspersonn@users.noreply.github.com>
2024-12-07 17:09:15 +0100

    Update service-vaultwarden.nix

--- modules/service-vaultwarden.nix
+++ modules/service-vaultwarden.nix
@@ -3,8 +3,26 @@
 {
   services.vaultwarden = {
     enable = true;
-    environmentFile = xxx; # Needs to be changed
-    config = xxx; # Needs to be changed
+    #environmentFile = xxx; # Needs to be changed
+    #config = xxx; # Needs to be changed
   }
+
+security.acme.defaults.email = "";
+security.acme.acceptTerms = true;
+
+services.nginx = {
+    enable = true;
+    
+    # Use recommended settings
+    recommendedGzipSettings = true;
+
+    virtualHosts."vaultwarden.home.com" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:8000";
+      };
+    };
+};
 
 }
