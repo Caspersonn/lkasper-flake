21c4c6e2fa5cf6b2f205586b521f2bf18465a101
Caspersonn <lucakasper8@gmail.com>
2025-08-08 21:55:53 +0200

    Addition of atuin

--- home/programs/atuin.nix
+++ home/programs/atuin.nix
@@ -7,7 +7,7 @@
         settings = {
             auto_sync = true;
             sync_frequency = "5m";
-            sync_address = "https://api.atuin.sh";
+            sync_address = "https://atuin.inspiravita.com";
             search_mode = "prefix";
         };
     };
