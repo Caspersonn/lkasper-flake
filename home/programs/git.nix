d58d07e48716f2bb93d9bd077b9c1938e83ad9cb
Caspersonn <lucakasper8@gmail.com>
2026-02-03 08:49:44 +0100

    Fix git configuration deprecated options

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -1,15 +1,20 @@
-{...}:
+{ ... }:
 
 {
+  # Git configuration  
   programs.git = {
     enable = true;
-    userEmail = "lucakasper8@gmail.com";
-    userName = "Caspersonn";
-    extraConfig = {
-      push = { autoSetupRemote = true; };
+    settings = {
+      user = {
+        email = "lucakasper8@gmail.com";
+        name = "Caspersonn";
+      };
+      push = {
+        autoSetupRemote = true;
+        default = "simple";
+      };
       branch = { autosetupmerge = true; };
-      push = { default = "simple"; };
-      pull = { rebase = true;};
+      pull = { rebase = true; };
       merge = { tool = "splice"; };
     };
   };
