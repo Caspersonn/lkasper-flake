147ee3681ceebe15f1dbb0603baa9f414e7a968d
Caspersonn <lucakasper8@gmail.com>
2026-02-05 20:22:27 +0100

    auto run after nixos-rebuild switch

--- modules/users/casper.nix
+++ modules/users/casper.nix
@@ -6,6 +6,9 @@
       description = "Luca Kasper";
       extraGroups = [ "networkmanager" "wheel" "docker" "disk" ];
       shell = pkgs.zsh;
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOtpGyC5u8+T71Oo+QL9ym+hWaNSiisskL43ElmpWiEr"
+      ];
     };
     users.defaultUserShell = pkgs.zsh;
     programs.zsh.enable = true;
