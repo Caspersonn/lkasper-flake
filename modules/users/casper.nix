1522f1af0b5d0d37b40d7059862af0a5190aceea
Caspersonn <lucakasper8@gmail.com>
2026-02-07 14:32:04 +0100

    auto run after nixos-rebuild switch

--- modules/users/casper.nix
+++ modules/users/casper.nix
@@ -12,5 +12,8 @@
     };
     users.defaultUserShell = pkgs.zsh;
     programs.zsh.enable = true;
+
+    nix.settings.require-sigs = false;
+    nix.settings.trusted-public-keys = ["gaming-casper:KYvnn1Wx4w2rbz7dQQHUvHu+0N5NxsDlLkSpIX3WGeM="];
   };
 }
