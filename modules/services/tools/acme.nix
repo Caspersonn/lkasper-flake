16834b900f3ed82d990a4d15f1e1062bad9f5643
Caspersonn <lucakasper8@gmail.com>
2026-02-07 21:43:47 +0100

    Update

--- modules/services/tools/acme.nix
+++ modules/services/tools/acme.nix
@@ -1,5 +1,11 @@
 { inputs, ... }: {
   flake.modules.nixos.acme = { config, pkgs, ... }: {
+
+    environment.systemPackages = with pkgs; [
+      acme
+      lego
+    ];
+
     security.acme = {
       acceptTerms = true;
       defaults.email = "security@inspiravita.com";
