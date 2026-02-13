d8feb095d3ea5199810aa9f013541184b0c7c676
Caspersonn <lucakasper8@gmail.com>
2026-02-13 17:53:41 +0100

    update

---
+++ modules/hosts/neko/configuration.nix
@@ -0,0 +1,74 @@
+{ inputs, self, ... }:
+
+let hostname = "neko";
+
+in {
+  flake.nixosConfigurations = {
+    neko = self.lib.makeNixos {
+      inherit hostname;
+      system = "x86_64-linux";
+      enableFrameworkHardware = false;
+    };
+  };
+
+  flake.homeConfigurations = {
+    "casper@${hostname}" = self.lib.makeHomeConf {
+      inherit hostname;
+      imports = with inputs.self.modules.homeManager; [ casper ];
+    };
+  };
+
+  flake.modules.nixos.neko = { config, pkgs, lib, ... }: {
+    imports = with inputs.self.modules.nixos; [
+      # System Configuration
+      system-default
+      locale
+      networking
+      audio
+      openssh
+      nixpkgs
+      age
+
+      # Home Manager
+      hm-nixos
+      hm-users
+
+      # Programs - CLI Tools
+      cli-tools
+
+      # Programs - Server
+      server-tools
+
+      # Services
+      resolved
+      tailscale
+      docker
+      smb
+      atuin
+      bluetooth-receiver
+      postgres
+
+      # Tools
+      pokemon-tracker
+      acme
+      vaultwarden
+      nextcloud
+
+      # System
+      secrets
+    ];
+
+    # State version
+    system.stateVersion = "25.11";
+
+    networking.hostName = "neko";
+
+    services.xserver.displayManager.gdm.autoSuspend = false;
+    systemd.targets.sleep.enable = false;
+    systemd.targets.suspend.enable = false;
+    systemd.targets.hibernate.enable = false;
+    systemd.targets.hybrid-sleep.enable = false;
+
+    services.pipewire.jack.enable = true;
+  };
+}
