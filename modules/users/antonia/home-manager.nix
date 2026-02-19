6746feba5c2f7e01ce66e016fa1d9c8d05aa2ec2
Caspersonn <lucakasper8@gmail.com>
2026-02-19 14:19:54 +0100

    Update

--- modules/users/antonia/home-manager.nix
+++ modules/users/antonia/home-manager.nix
@@ -4,6 +4,9 @@
     imports = with inputs.self.modules.homeManager; (with inputs.omarchy-nix.homeManagerModules; [
       inputs.plasma-manager.homeModules.plasma-manager
       kde
+      casper-firefox
+      casper-zsh
+      casper-neovim
     ]);
 
     nixpkgs.config.allowUnfree = true;
