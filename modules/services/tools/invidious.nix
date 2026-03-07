cccc38b5a7eac37703c7de0084f16d22bf2e1a3f
Caspersonn <lucakasper8@gmail.com>
2026-03-07 13:29:40 +0100

    update

--- modules/services/tools/invidious.nix
+++ modules/services/tools/invidious.nix
@@ -49,11 +49,17 @@
       recommendedProxySettings = true;
       recommendedTlsSettings = true;
       clientMaxBodySize = "40M";
+      appendHttpConfig = ''
+        proxy_buffer_size 16k;
+        proxy_buffers 4 16k;
+        proxy_busy_buffers_size 16k;
+        large_client_header_buffers 4 16k;
+      '';
       virtualHosts."invidious.inspiravita.com" = {
         forceSSL = true;
         enableACME = true;
-        locations."/companion/" = {
-          proxyPass = "http://127.0.0.1:8282/companion/";
+        locations."/companion" = {
+          proxyPass = "http://127.0.0.1:8282";
           proxyWebsockets = true;
           extraConfig = ''
             auth_basic off;
@@ -64,22 +70,15 @@
             proxy_send_timeout 3600s;
             proxy_connect_timeout 60s;
 
-            proxy_hide_header 'Access-Control-Allow-Origin';
-            proxy_hide_header 'Access-Control-Allow-Methods';
-            proxy_hide_header 'Access-Control-Allow-Headers';
-            proxy_hide_header 'Access-Control-Allow-Credentials';
-
             add_header 'Access-Control-Allow-Origin' 'https://materialious.inspiravita.com' always;
-            add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS, HEAD, PUT, DELETE, PATCH' always;
+            add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS' always;
             add_header 'Access-Control-Allow-Headers' 'DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range,Authorization' always;
-            add_header 'Access-Control-Allow-Credentials' 'true' always;
             add_header 'Access-Control-Expose-Headers' 'Content-Length,Content-Range' always;
 
             if ($request_method = 'OPTIONS') {
               add_header 'Access-Control-Allow-Origin' 'https://materialious.inspiravita.com';
-              add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS, HEAD, PUT, DELETE, PATCH';
+              add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
               add_header 'Access-Control-Allow-Headers' 'DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range,Authorization';
-              add_header 'Access-Control-Allow-Credentials' 'true';
               add_header 'Access-Control-Max-Age' 1728000;
               add_header 'Content-Type' 'text/plain; charset=utf-8';
               add_header 'Content-Length' 0;
