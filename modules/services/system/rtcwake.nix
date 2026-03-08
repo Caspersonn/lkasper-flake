6d27eeee7a8e469a3906d130532999f9c18f1fc2
Caspersonn <lucakasper8@gmail.com>
2026-03-08 12:54:25 +0100

    auto run after nixos-rebuild switch

--- modules/services/system/rtcwake.nix
+++ modules/services/system/rtcwake.nix
@@ -2,16 +2,15 @@
   flake.modules.nixos.rtcwake = { pkgs, ... }: {
 
     systemd.services.rtcwake-hibernate = {
-      description = "Hibernate and wake at 7 AM";
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart =
-          "${pkgs.util-linux}/bin/rtcwake -m disk -l -t $(${pkgs.coreutils}/bin/date +%%s -d 'tomorrow 07:00')";
-      };
+      description = "Hibernate and wake at 7 AM next day";
+      script = ''
+        ${pkgs.util-linux}/bin/rtcwake -m disk -l -t $(${pkgs.coreutils}/bin/date +%s -d 'tomorrow 07:00')
+      '';
+      serviceConfig.Type = "oneshot";
     };
 
     systemd.timers.rtcwake-hibernate = {
-      description = "Hibernate at 10 PM daily";
+      description = "Hibernate at 10:00 PM daily";
       wantedBy = [ "timers.target" ];
       timerConfig = {
         OnCalendar = "*-*-* 22:00:00";
