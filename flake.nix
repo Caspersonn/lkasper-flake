d64da605e92973478a860c8ad2710a1d279a0ea8
Caspersonn <lucakasper8@gmail.com>
2026-04-03 15:21:57 +0200

    Update

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
     slack2zammad.url = "github:wearetechnative/slack2zammad";
     dirty-repo-scanner.url = "github:mipmip/dirty-repo-scanner";
     swww.url = "github:LGFae/swww";
-    nixvim.url = "/home/casper/git/personal/nixvim";
+    nixvim.url = "github:Caspersonn/nixvim";
     openspec.url = "github:Fission-AI/OpenSpec";
     spicetify-nix.url = "github:Gerg-L/spicetify-nix";
     ssmsh.url = "github:torreirow/ssmsh";
