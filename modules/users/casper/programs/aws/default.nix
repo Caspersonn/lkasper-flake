9c159bed0d151a4e5acbc17d0bb9484ce2be36b7
Caspersonn <lucakasper8@gmail.com>
2026-04-14 14:20:24 +0200

    auto run after nixos-rebuild switch

--- modules/users/casper/programs/aws/default.nix
+++ modules/users/casper/programs/aws/default.nix
@@ -65,6 +65,7 @@
         "255217714588" = "eu-west-1"; # DocRevolution nonprod
         "189796657102" = "eu-west-1"; # DocRevolution Shared services
         "238020582243" = "eu-west-1"; # DocRevolution Shared services
+        "562622988252" = "us-east-1"; # Mustad management
       };
       alternative_names = {
         #"760178553019" = "pg_wtoorren";
--- modules/users/casper/programs/dirtygit/default.nix
+++
@@ -1,7 +0,0 @@
-{ ... }: {
-  flake.modules.homeManager.casper-dirtygit = { inputs, pkgs, ... }: {
-    home.packages = [ inputs.dirty-repo-scanner.packages."${pkgs.system}".dirty-repo-scanner ];
-
-    home.file = { "./.dirtygit.yml" = { source = ./dirtygit.yml; }; };
-  };
-}
