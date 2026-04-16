7d65c53c218389e6e6069cc5275e45c2609c76d9
Caspersonn <lucakasper8@gmail.com>
2026-04-16 14:19:57 +0200

    Add arcana-one

---
+++ modules/hosts/arcana-one/configuration.nix
@@ -0,0 +1,21 @@
+{ inputs, self, ... }:
+
+let hostname = "arcana-one";
+
+in {
+
+  flake.homeConfigurations = {
+    "casper@${hostname}" = self.lib.makeHomeConf {
+      inherit hostname;
+      imports = with inputs.self.modules.homeManager; [ casper ];
+    };
+  };
+
+  flake.modules.nixos.gaming-casper = { config, pkgs, lib, ... }: {
+    imports = with inputs.self.modules.nixos; [
+      # Home manager
+      hm-nixos
+      hm-users
+    ];
+  };
+}
