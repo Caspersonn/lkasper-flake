a440530726cf40f46977f0d09c6178a123007a9b
Caspersonn <lucakasper8@gmail.com>
2026-04-18 19:37:34 +0200

    Add kaito

---
+++ modules/hosts/kaito/configuration.nix
@@ -0,0 +1,81 @@
+{ inputs, self, ... }:
+
+let hostname = "kaito";
+
+in {
+  flake.nixosConfigurations = {
+    neko = self.lib.makeNixos {
+      inherit hostname;
+      system = "x86_64-linux";
+      enableFrameworkHardware = false;
+    };
+  };
+
+  flake.homeConfigurations = {
+    "casper@${hostname}" = self.lib.makeHomeConf {
+      inherit hostname;
+      imports = with inputs.self.modules.homeManager; [ casper ];
+    };
+  };
+
+  flake.modules.nixos.neko = { config, pkgs, lib, ... }: {
+    imports = with inputs.self.modules.nixos; [
+      # System Configuration
+      system-default
+      locale
+      boot
+      graphics
+      networking
+      audio
+      openssh
+      nixpkgs
+      age
+
+      # Home Manager
+      hm-nixos
+      hm-users
+      casper
+
+      # Programs - CLI Tools
+      cli-tools
+
+      # Programs - Server
+      server-tools
+
+      # Services
+      resolved
+      tailscale
+      docker
+      smb
+      atuin
+      bluetooth-receiver
+      postgres
+      rtcwake
+
+      # Tools
+      pokemon-tracker
+      acme
+      vaultwarden
+      nextcloud
+      materialious
+      invidious
+
+      # System
+      secrets
+    ];
+
+    # State version
+    system.stateVersion = "25.11";
+
+    networking.hostName = hostname;
+    programs.dconf.enable = true;
+
+    services.xserver.displayManager.gdm.autoSuspend = false;
+    systemd.targets.sleep.enable = false;
+    systemd.targets.suspend.enable = false;
+    systemd.targets.hibernate.enable = false;
+    systemd.targets.hybrid-sleep.enable = false;
+
+    services.pipewire.jack.enable = true;
+  };
+}
