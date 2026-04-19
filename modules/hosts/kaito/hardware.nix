bd4e85bb1b3a3b5a02791c2c651a845f23012fb5
Caspersonn <lucakasper8@gmail.com>
2026-04-19 08:19:27 +0200

    Kaito

--- modules/hosts/kaito/hardware.nix
+++ modules/hosts/kaito/hardware.nix
@@ -1,34 +1,31 @@
-#{ lib, inputs, ... }: {
-#  flake.modules.nixos.neko = { config, pkgs, modulesPath, ... }: {
-#    imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
-#
-#    # Boot configuration
-#    boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
-#    boot.initrd.kernelModules = [ ];
-#    boot.kernelModules = [ ];
-#    boot.extraModulePackages = [ ];
-#
-#    # File systems
-#    fileSystems."/" =
-#    { device = "/dev/disk/by-uuid/f2b7fc23-61fd-4bc2-849a-5d8646ebca18";
-#	 fsType = "ext4";
-#    };
-#
-#    fileSystems."/boot" =
-#    { device = "/dev/disk/by-uuid/3B31-57A1";
-#	    fsType = "vfat";
-#	    options = [ "fmask=0077" "dmask=0077" ];
-#    };
-#
-#    swapDevices =
-#	    [ { device = "/dev/disk/by-uuid/11bc44fe-e525-4ee7-be08-be15a2fe8eb4"; }
-#	    ];
-#
-#    # Networking
-#    networking.useDHCP = lib.mkDefault true;
-#
-#    # Platform (ARM)
-#    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-#    hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-#  };
-#}
+{ config, lib, pkgs, modulesPath, ... }: {
+
+flake.modules.nixos.kaito = { config, pkgs, modulesPath, ... }: {
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/afeade7f-d6ef-49d5-b71f-0fc1784a8607";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/FC07-E626";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/cf484223-7297-4331-8372-fc5415450828"; }
+    ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+};
+}
