201010920bfdd77c1f0d914c83124457ad4e29b8
Caspersonn <lucakasper8@gmail.com>
2026-06-21 21:17:35 +0200

    Navidrome update

--- modules/services/tools/navidrome.nix
+++ modules/services/tools/navidrome.nix
@@ -1,8 +1,11 @@
 { config, ... }: {
-flake.modules.nixos.navidrome = {
+flake.modules.nixos.navidrome = {pkgs, ...}: {
     services.navidrome = {
       enable = true;
       settings.MusicFolder = "/mnt/audio/music";
+      plugins = with pkgs; [
+          listenbrainz-daily-playlist
+      ];
     };
   };
 }
