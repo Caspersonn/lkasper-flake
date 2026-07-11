63cddbbe2bcbaea7e9734c50fe6051d3bc614e2f
Caspersonn <lucakasper8@gmail.com>
2026-07-11 12:07:22 +0200

    feat: update navidrome

--- modules/services/tools/navidrome.nix
+++ modules/services/tools/navidrome.nix
@@ -11,5 +11,19 @@ flake.modules.nixos.navidrome = {pkgs, ...}: {
           listenbrainz-daily-playlist
       ];
     };
+
+    # Allow the CD-rip watcher on the laptop to push finished albums into
+    # navidrome's music folder over ssh. It rsyncs as user casper and
+    # invokes rsync on this host through `sudo -n -u navidrome` so files
+    # land owned by the navidrome user directly.
+    environment.systemPackages = [ pkgs.rsync ];
+    security.sudo.extraRules = [{
+      users = [ "casper" ];
+      runAs = "navidrome";
+      commands = [{
+        command = "${pkgs.rsync}/bin/rsync";
+        options = [ "NOPASSWD" ];
+      }];
+    }];
   };
 }
