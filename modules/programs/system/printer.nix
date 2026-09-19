612fb320920bdd924f97f8c8cd9ef9a34537eff9
Caspersonn <lucakasper8@gmail.com>
2026-09-19 23:14:00 +0200

    Move hardware printer to own fil;e

--- modules/programs/system/printer.nix
+++ modules/programs/system/printer.nix
@@ -1,11 +1,11 @@
 { inputs, ... }: {
   flake.modules.nixos.hardware-utils = { pkgs, ... }: {
     environment.systemPackages = with pkgs; [
-      android-tools
-      ddcui
-      libusb1
-      usbutils
-      sof-firmware
+      open-scq30
+      epson-escpr
+      epson-escpr2
+      simple-scan
+      sane-airscan
     ];
 
     hardware.sane = {
@@ -13,12 +13,10 @@
       extraBackends = [ pkgs.sane-airscan ];
     };
 
-    # mDNS/DNS-SD discovery for network devices
     services.avahi = {
       enable = true;
       openFirewall = true;
     };
-
 
     users.users.casper.extraGroups = [
       "scanner"
