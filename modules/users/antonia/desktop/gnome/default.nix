f5448161c57ee2399509096c30f9c4822dd0532c
antonia <antoniagosker@gmail.com>
2026-09-19 23:27:02 +0200

    merge fix

--- modules/users/antonia/desktop/gnome/default.nix
+++ modules/users/antonia/desktop/gnome/default.nix
@@ -1,5 +1,5 @@
 { inputs, ... }: {
-  flake.modules.homeManager.anotnia-gnome = { pkgs, lib, ... }:
+  flake.modules.homeManager.antonia-gnome = { pkgs, lib, ... }:
     let self-pkgs = inputs.self.packages.${pkgs.system};
     in {
       home.packages = with pkgs; [
